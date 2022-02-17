486ad7a9d067265c143f19bc9d00a14fcb21d810
AlexWT1 <99095805+AlexWT1@users.noreply.github.com>
2022-02-17 21:17:36 +0300

    Добавьте файлы проекта.

---
+++ laboratory_work_1/Form1.Designer.cs
@@ -0,0 +1,364 @@
+﻿namespace laboratory_work_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.l5_1_11 = new System.Windows.Forms.Label();
+            this.tb4_1_11 = new System.Windows.Forms.TextBox();
+            this.tb3_1_11 = new System.Windows.Forms.TextBox();
+            this.tb2_1_11 = new System.Windows.Forms.TextBox();
+            this.tb1_1_11 = new System.Windows.Forms.TextBox();
+            this.btn2_1_11 = new System.Windows.Forms.Button();
+            this.btn1_1_11 = new System.Windows.Forms.Button();
+            this.l4_1_11 = new System.Windows.Forms.Label();
+            this.l3_1_11 = new System.Windows.Forms.Label();
+            this.l2_1_11 = new System.Windows.Forms.Label();
+            this.l1_1_11 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.l4_1_12 = new System.Windows.Forms.Label();
+            this.tb3_1_12 = new System.Windows.Forms.TextBox();
+            this.tb2_1_12 = new System.Windows.Forms.TextBox();
+            this.tb1_1_12 = new System.Windows.Forms.TextBox();
+            this.l3_1_12 = new System.Windows.Forms.Label();
+            this.l2_1_12 = new System.Windows.Forms.Label();
+            this.btn2_1_12 = new System.Windows.Forms.Button();
+            this.l1_1_12 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.btn1_1_12 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(859, 497);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Gray;
+            this.tabPage1.Controls.Add(this.l5_1_11);
+            this.tabPage1.Controls.Add(this.tb4_1_11);
+            this.tabPage1.Controls.Add(this.tb3_1_11);
+            this.tabPage1.Controls.Add(this.tb2_1_11);
+            this.tabPage1.Controls.Add(this.tb1_1_11);
+            this.tabPage1.Controls.Add(this.btn2_1_11);
+            this.tabPage1.Controls.Add(this.btn1_1_11);
+            this.tabPage1.Controls.Add(this.l4_1_11);
+            this.tabPage1.Controls.Add(this.l3_1_11);
+            this.tabPage1.Controls.Add(this.l2_1_11);
+            this.tabPage1.Controls.Add(this.l1_1_11);
+            this.tabPage1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(851, 468);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Задание 1.11";
+            // 
+            // l5_1_11
+            // 
+            this.l5_1_11.AutoSize = true;
+            this.l5_1_11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l5_1_11.ForeColor = System.Drawing.Color.Red;
+            this.l5_1_11.Location = new System.Drawing.Point(32, 18);
+            this.l5_1_11.Name = "l5_1_11";
+            this.l5_1_11.Size = new System.Drawing.Size(0, 25);
+            this.l5_1_11.TabIndex = 14;
+            // 
+            // tb4_1_11
+            // 
+            this.tb4_1_11.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb4_1_11.Location = new System.Drawing.Point(413, 249);
+            this.tb4_1_11.Name = "tb4_1_11";
+            this.tb4_1_11.Size = new System.Drawing.Size(100, 34);
+            this.tb4_1_11.TabIndex = 13;
+            // 
+            // tb3_1_11
+            // 
+            this.tb3_1_11.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb3_1_11.Location = new System.Drawing.Point(413, 193);
+            this.tb3_1_11.Name = "tb3_1_11";
+            this.tb3_1_11.Size = new System.Drawing.Size(100, 34);
+            this.tb3_1_11.TabIndex = 12;
+            // 
+            // tb2_1_11
+            // 
+            this.tb2_1_11.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb2_1_11.Location = new System.Drawing.Point(88, 121);
+            this.tb2_1_11.Name = "tb2_1_11";
+            this.tb2_1_11.Size = new System.Drawing.Size(100, 34);
+            this.tb2_1_11.TabIndex = 11;
+            // 
+            // tb1_1_11
+            // 
+            this.tb1_1_11.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb1_1_11.Location = new System.Drawing.Point(87, 61);
+            this.tb1_1_11.Name = "tb1_1_11";
+            this.tb1_1_11.Size = new System.Drawing.Size(100, 34);
+            this.tb1_1_11.TabIndex = 10;
+            // 
+            // btn2_1_11
+            // 
+            this.btn2_1_11.BackColor = System.Drawing.Color.IndianRed;
+            this.btn2_1_11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn2_1_11.Location = new System.Drawing.Point(227, 313);
+            this.btn2_1_11.Name = "btn2_1_11";
+            this.btn2_1_11.Size = new System.Drawing.Size(220, 111);
+            this.btn2_1_11.TabIndex = 9;
+            this.btn2_1_11.Text = "Очистить";
+            this.btn2_1_11.UseVisualStyleBackColor = false;
+            this.btn2_1_11.Click += new System.EventHandler(this.btn1_1_11_Click);
+            // 
+            // btn1_1_11
+            // 
+            this.btn1_1_11.BackColor = System.Drawing.Color.Yellow;
+            this.btn1_1_11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn1_1_11.Location = new System.Drawing.Point(37, 313);
+            this.btn1_1_11.Name = "btn1_1_11";
+            this.btn1_1_11.Size = new System.Drawing.Size(150, 111);
+            this.btn1_1_11.TabIndex = 8;
+            this.btn1_1_11.Text = "Вычислить";
+            this.btn1_1_11.UseVisualStyleBackColor = false;
+            this.btn1_1_11.Click += new System.EventHandler(this.btn2_1_11_Click);
+            // 
+            // l4_1_11
+            // 
+            this.l4_1_11.AutoSize = true;
+            this.l4_1_11.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l4_1_11.Location = new System.Drawing.Point(32, 254);
+            this.l4_1_11.Name = "l4_1_11";
+            this.l4_1_11.Size = new System.Drawing.Size(355, 29);
+            this.l4_1_11.TabIndex = 6;
+            this.l4_1_11.Text = "Среднее геометрическое =";
+            // 
+            // l3_1_11
+            // 
+            this.l3_1_11.AutoSize = true;
+            this.l3_1_11.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l3_1_11.Location = new System.Drawing.Point(32, 198);
+            this.l3_1_11.Name = "l3_1_11";
+            this.l3_1_11.Size = new System.Drawing.Size(365, 29);
+            this.l3_1_11.TabIndex = 5;
+            this.l3_1_11.Text = "Среднее арифметическое =";
+            // 
+            // l2_1_11
+            // 
+            this.l2_1_11.AutoSize = true;
+            this.l2_1_11.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l2_1_11.Location = new System.Drawing.Point(32, 126);
+            this.l2_1_11.Name = "l2_1_11";
+            this.l2_1_11.Size = new System.Drawing.Size(50, 29);
+            this.l2_1_11.TabIndex = 4;
+            this.l2_1_11.Text = "b =";
+            // 
+            // l1_1_11
+            // 
+            this.l1_1_11.AutoSize = true;
+            this.l1_1_11.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l1_1_11.Location = new System.Drawing.Point(32, 64);
+            this.l1_1_11.Name = "l1_1_11";
+            this.l1_1_11.Size = new System.Drawing.Size(49, 29);
+            this.l1_1_11.TabIndex = 0;
+            this.l1_1_11.Text = "a =";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.Gray;
+            this.tabPage2.Controls.Add(this.btn1_1_12);
+            this.tabPage2.Controls.Add(this.l4_1_12);
+            this.tabPage2.Controls.Add(this.tb3_1_12);
+            this.tabPage2.Controls.Add(this.tb2_1_12);
+            this.tabPage2.Controls.Add(this.tb1_1_12);
+            this.tabPage2.Controls.Add(this.l3_1_12);
+            this.tabPage2.Controls.Add(this.l2_1_12);
+            this.tabPage2.Controls.Add(this.btn2_1_12);
+            this.tabPage2.Controls.Add(this.l1_1_12);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(851, 468);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Задание 1.12";
+            // 
+            // l4_1_12
+            // 
+            this.l4_1_12.AutoSize = true;
+            this.l4_1_12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l4_1_12.ForeColor = System.Drawing.Color.Red;
+            this.l4_1_12.Location = new System.Drawing.Point(56, 33);
+            this.l4_1_12.Name = "l4_1_12";
+            this.l4_1_12.Size = new System.Drawing.Size(0, 20);
+            this.l4_1_12.TabIndex = 17;
+            // 
+            // tb3_1_12
+            // 
+            this.tb3_1_12.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb3_1_12.Location = new System.Drawing.Point(106, 204);
+            this.tb3_1_12.Name = "tb3_1_12";
+            this.tb3_1_12.Size = new System.Drawing.Size(100, 34);
+            this.tb3_1_12.TabIndex = 16;
+            // 
+            // tb2_1_12
+            // 
+            this.tb2_1_12.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb2_1_12.Location = new System.Drawing.Point(106, 145);
+            this.tb2_1_12.Name = "tb2_1_12";
+            this.tb2_1_12.Size = new System.Drawing.Size(100, 34);
+            this.tb2_1_12.TabIndex = 15;
+            // 
+            // tb1_1_12
+            // 
+            this.tb1_1_12.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb1_1_12.Location = new System.Drawing.Point(106, 83);
+            this.tb1_1_12.Name = "tb1_1_12";
+            this.tb1_1_12.Size = new System.Drawing.Size(100, 34);
+            this.tb1_1_12.TabIndex = 14;
+            // 
+            // l3_1_12
+            // 
+            this.l3_1_12.AutoSize = true;
+            this.l3_1_12.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l3_1_12.Location = new System.Drawing.Point(51, 209);
+            this.l3_1_12.Name = "l3_1_12";
+            this.l3_1_12.Size = new System.Drawing.Size(48, 29);
+            this.l3_1_12.TabIndex = 13;
+            this.l3_1_12.Text = "c =";
+            // 
+            // l2_1_12
+            // 
+            this.l2_1_12.AutoSize = true;
+            this.l2_1_12.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l2_1_12.Location = new System.Drawing.Point(51, 150);
+            this.l2_1_12.Name = "l2_1_12";
+            this.l2_1_12.Size = new System.Drawing.Size(50, 29);
+            this.l2_1_12.TabIndex = 12;
+            this.l2_1_12.Text = "b =";
+            // 
+            // btn2_1_12
+            // 
+            this.btn2_1_12.BackColor = System.Drawing.Color.IndianRed;
+            this.btn2_1_12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn2_1_12.Location = new System.Drawing.Point(247, 282);
+            this.btn2_1_12.Name = "btn2_1_12";
+            this.btn2_1_12.Size = new System.Drawing.Size(220, 111);
+            this.btn2_1_12.TabIndex = 11;
+            this.btn2_1_12.Text = "Очистить";
+            this.btn2_1_12.UseVisualStyleBackColor = false;
+            this.btn2_1_12.Click += new System.EventHandler(this.btn2_1_12_Click);
+            // 
+            // l1_1_12
+            // 
+            this.l1_1_12.AutoSize = true;
+            this.l1_1_12.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l1_1_12.Location = new System.Drawing.Point(51, 88);
+            this.l1_1_12.Name = "l1_1_12";
+            this.l1_1_12.Size = new System.Drawing.Size(49, 29);
+            this.l1_1_12.TabIndex = 0;
+            this.l1_1_12.Text = "a =";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.BackColor = System.Drawing.Color.DimGray;
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(851, 468);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Задание 1.12";
+            // 
+            // btn1_1_12
+            // 
+            this.btn1_1_12.BackColor = System.Drawing.Color.Yellow;
+            this.btn1_1_12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn1_1_12.Location = new System.Drawing.Point(56, 282);
+            this.btn1_1_12.Name = "btn1_1_12";
+            this.btn1_1_12.Size = new System.Drawing.Size(150, 111);
+            this.btn1_1_12.TabIndex = 18;
+            this.btn1_1_12.Text = "Вычислить";
+            this.btn1_1_12.UseVisualStyleBackColor = false;
+            this.btn1_1_12.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 497);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Лабораторная 1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TextBox tb4_1_11;
+        private System.Windows.Forms.TextBox tb3_1_11;
+        private System.Windows.Forms.TextBox tb2_1_11;
+        private System.Windows.Forms.TextBox tb1_1_11;
+        private System.Windows.Forms.Button btn2_1_11;
+        private System.Windows.Forms.Button btn1_1_11;
+        private System.Windows.Forms.Label l4_1_11;
+        private System.Windows.Forms.Label l3_1_11;
+        private System.Windows.Forms.Label l2_1_11;
+        private System.Windows.Forms.Label l1_1_11;
+        private System.Windows.Forms.Label l5_1_11;
+        private System.Windows.Forms.Label l4_1_12;
+        private System.Windows.Forms.TextBox tb3_1_12;
+        private System.Windows.Forms.TextBox tb2_1_12;
+        private System.Windows.Forms.TextBox tb1_1_12;
+        private System.Windows.Forms.Label l3_1_12;
+        private System.Windows.Forms.Label l2_1_12;
+        private System.Windows.Forms.Button btn2_1_12;
+        private System.Windows.Forms.Label l1_1_12;
+        private System.Windows.Forms.Button btn1_1_12;
+    }
+}
+
---
+++ laboratory_work_1/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laboratory_work_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double a, b, am, gm, c;
+
+       
+
+        private void btn2_1_11_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                a = Convert.ToDouble(tb1_1_11.Text);
+                b = Convert.ToDouble(tb2_1_11.Text);
+                am = (a + b);
+                gm = Math.Round(Math.Sqrt(a * b),1);
+                tb3_1_11.Text = Convert.ToString(am);
+                tb4_1_11.Text = Convert.ToString(gm);
+                l5_1_11.Text = null;
+            }
+            catch (FormatException)
+            {
+                l5_1_11.Text = "Упс, что-то не так , проверьте введеное значение.";
+            }
+        }
+
+        private void btn1_1_11_Click(object sender, EventArgs e)
+        {
+            tb1_1_11.Clear();
+            tb2_1_11.Clear();
+            tb3_1_11.Clear();
+            tb4_1_11.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                a = Convert.ToDouble(tb1_1_12.Text);
+                b = Convert.ToDouble(tb2_1_12.Text);
+                c = Math.Round(Math.Sqrt((a * a) + (b * b)),1);
+                tb3_1_12.Text = Convert.ToString(c);
+                l4_1_12.Text = null;
+            }
+            catch (FormatException)
+            {
+                l4_1_12.Text = "Упс, что-то не так , проверьте введеное значение.";
+            }
+        }
+
+        private void btn2_1_12_Click(object sender, EventArgs e)
+        {
+            tb1_1_12.Clear();
+            tb2_1_12.Clear();
+            tb3_1_12.Clear();
+        }
+    }
+}
